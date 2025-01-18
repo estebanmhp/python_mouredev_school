07c0065fdc2b12b5d9ffce43831e40ae1af3c376
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-18 17:48:12 -0500

    Add routers for authentication

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,7 +1,7 @@
 # API Basics
 
 from fastapi import FastAPI
-from Routers import products, users
+from Routers import products, users, basic_auth_users, jwt_auth_users
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -24,6 +24,9 @@ async def url_course():
 
 app.include_router(products.router)
 app.include_router(users.router)
+app.include_router(basic_auth_users.router)
+app.include_router(jwt_auth_users.router)
+
 
 # STATICS
 # Example -> http://127.0.0.1:8000/statics/Images/Python-01.jpg

bb8cc2827afb23e853946a328a4e8b9e87f7e139
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-19 10:22:31 -0500

    Connect with Mongo DB Atlas (Remote DataBase)

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,4 +1,7 @@
 from pymongo import MongoClient
 
-db_client = MongoClient().local # Connect by default local host
+# Data base (local)
+# db_client = MongoClient().local 
 
+# Data base (remote)
+db_client = MongoClient("mongodb+srv://test:test@usersdb.dqc69.mongodb.net/?retryWrites=true&w=majority&appName=UsersDB").test

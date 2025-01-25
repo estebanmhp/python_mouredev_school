cbb3c7273e1d6d0645fa7d3ba6348afee62b61a2
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-25 13:48:18 -0500

    Continue with the website of the basics course

---
+++ Web/Advanced/link_bio/link_bio/link_bio.py
@@ -0,0 +1,39 @@
+import reflex as rx
+from link_bio.components.navbar import navbar
+from link_bio.components.footer import footer
+from link_bio.views.header import header
+from link_bio.views.repo_links import repo_links
+import link_bio.styles.styles as styles
+from link_bio.styles.styles import Size as Size
+
+class State(rx.State):
+    pass
+
+# Graphic return (rx.Component)
+
+def index() -> rx.Component:
+    return rx.box(
+        navbar(),
+        rx.center(
+            rx.vstack(
+                header(),
+                repo_links(),            
+                align="center",
+                justify="center",
+                max_width = styles.MAX_WIDTH,
+                width = "100%",
+                margin_y = Size.BIG.value
+            ),
+        ),
+        footer()
+    )
+
+app = rx.App(
+    style = styles.BASE_STYLE
+)
+app.add_page(
+    index,
+    title = "Esteban Hernandez - Passionate Developer",
+    description = "Developer focused on building projects, honing skills, and embracing new technologies every day",
+    image = "/images/logo.png"
+)

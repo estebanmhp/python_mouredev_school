d09c784bdf1c9365af23898f9e7463dc5e2f474e
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-25 13:32:31 -0500

    Start Over

--- Web/Advanced/link_bio/link_bio/components/title.py
+++
@@ -1,8 +0,0 @@
-import reflex as rx
-import link_bio.styles.styles as styles
-
-def title(name: str) -> rx.Component:
-    return rx.heading(
-        name,
-        style = styles.title_style
-    )

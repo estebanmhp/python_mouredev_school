219f88d2b2fa698f5a4fe11ba36ebba198fe50c3
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-25 23:23:53 -0500

    Test #3

--- Web/Advanced/link_bio/rxconfig.py
+++ Web/Advanced/link_bio/rxconfig.py
@@ -2,10 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
-    backend_host = "https://esteban-web.up.railway.app",
-    # cors_allowed_origins = [
-    #     "http://localhost:3000",
-    #     "https://estebanhernandezweb.vercel.app"        
-    # ],
-    is_production = True
+    backend_host="https://esteban-web.up.railway.app",
+    is_production=True
 )

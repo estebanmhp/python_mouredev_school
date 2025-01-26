9a549cfcae97a0adfd226e871e0d8c4bc69154db
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-25 23:28:59 -0500

    Test #4

--- Web/Advanced/link_bio/rxconfig.py
+++ Web/Advanced/link_bio/rxconfig.py
@@ -2,6 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
-    backend_host="https://esteban-web.up.railway.app",
+    api_url="https://esteban-web.up.railway.app",
     is_production=True
 )

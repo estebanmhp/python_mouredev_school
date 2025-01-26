96fa14d3b769cf39498712a20d2e25b9f00c89d3
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-25 22:11:30 -0500

    First deployment with backend

--- Web/Advanced/link_bio/rxconfig.py
+++ Web/Advanced/link_bio/rxconfig.py
@@ -1,5 +1,8 @@
 import reflex as rx
+import os
 
 config = rx.Config(
     app_name="link_bio",
+    backend_host = os.getenv("API_URL", "https://esteban-web.up.railway.app"),
+    is_production = True
 )

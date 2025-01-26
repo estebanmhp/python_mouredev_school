23950f71cbbad56c176a3b818cf6e7fcd56b40c7
estebanmhp <estebanhernandezpe@gmail.com>
2025-01-25 23:00:50 -0500

    CORS

--- Web/Advanced/link_bio/rxconfig.py
+++ Web/Advanced/link_bio/rxconfig.py
@@ -4,5 +4,9 @@ import os
 config = rx.Config(
     app_name="link_bio",
     backend_host = os.getenv("API_URL", "https://esteban-web.up.railway.app"),
+    cors_allowed_origins = [
+        "http://localhost:3000",
+        "https://estebanhernandezweb.vercel.app/"        
+    ],
     is_production = True
 )
